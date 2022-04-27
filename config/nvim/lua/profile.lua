34362f2e422b17fdf27a61844e5156f5c0c1e4cb
Shih-Ming Wang <swang150@ucsc.edu>
2022-04-27 11:53:12 -0700

    Support for profile global options.

--- config/nvim/lua/profile.lua
+++ config/nvim/lua/profile.lua
@@ -6,8 +6,11 @@ M.name = "default"
 M.envs = {}
 M.cur_env = M
 
-function M.register_env(env, mode_mappings)
-	mode_mappings = mode_mappings or {}
+function M.register_env(opts)
+	vim.validate({ env = { opts.env, "t" }, mode_mappings = { opts.mode_mappings, "t", true } })
+	local env = opts.env
+	local mode_mappings = opts.mode_mappings or {}
+	local default_env = M
 	M.envs[env.name] = env
 	env.mappings = {}
 	for mode, mappings in pairs(mode_mappings) do
@@ -17,12 +20,26 @@ function M.register_env(env, mode_mappings)
 			end, vim.tbl_values(mappings)),
 			mode
 		)
-		M.mappings = vim.tbl_extend("force", M.mappings, saved_mappings)
+		default_env.mappings = vim.tbl_extend("force", default_env.mappings, saved_mappings)
 		for _, mapping in pairs(mappings) do
 			mapping.mode = mode
 			mapping.buffer = -1
 			table.insert(env.mappings, mapping)
 		end
+	end
+
+	env.g = env.g or {}
+	default_env.g = default_env.g or {}
+	for k, v in pairs(env.g) do
+		local default_val
+		if type(v) == "number" then
+			default_val = 0
+		elseif type(v) == "boolean" then
+			default_val = false
+		elseif type(v) == "string" then
+			default_val = ""
+		end
+		default_env.g[k] = vim.g[k] or default_val
 	end
 end
 
@@ -41,12 +58,21 @@ function M.switch_env(env)
 		return
 	end
 	M.cur_env = env
+	local default_env = M
+
 	-- Restore default mappings and then apply current env mappings.
-	V.restore_keymap(M.mappings)
+	V.restore_keymap(default_env.mappings)
 	V.restore_keymap(M.cur_env.mappings)
+
+	for k, v in pairs(default_env.g) do
+		vim.g[k] = v
+	end
+	for k, v in pairs(M.cur_env.g) do
+		vim.g[k] = v
+	end
 end
 
-M.register_env(M)
+M.register_env({ env = M })
 vim.api.nvim_exec(
 	[[
 augroup PROFILE
