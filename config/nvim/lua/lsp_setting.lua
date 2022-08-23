0fdfbcdd9c2232ecbdd256efa2f0c1709b5fc5f4
Shih-Ming Wang <swang150@ucsc.edu>
2022-08-23 16:39:04 -0700

    update

--- config/nvim/lua/lsp_setting.lua
+++ config/nvim/lua/lsp_setting.lua
@@ -78,7 +78,7 @@ function M.lsp_diagnostic_open(line_number)
 		vim.fn.search(string.format("|%d col", line_number), "cw")
 	end, 10)
 end
-add_util_menu("LspDiagnosticOpen", vim.lsp.diagnostic.set_loclist)
+add_util_menu("LspDiagnosticOpen", vim.diagnostic.setloclist)
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 if pcall(require, "cmp_nvim_lsp") then
