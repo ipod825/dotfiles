45d56e01ce39885bb0d15b32ab54ea7d4fba56a0
Shih-Ming Wang <swang150@ucsc.edu>
2023-01-11 03:08:16 -0800

    update

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,4 +1,90 @@
 return {
+	{
+		"nvim-treesitter/playground",
+		cmd = "TSPlaygroundToggle",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				playground = {
+					enable = true,
+					disable = {},
+					updatetime = 25,
+					persist_queries = false,
+				},
+			})
+		end,
+	},
+
+	{
+		"mfussenegger/nvim-treehopper",
+		keys = { { "m", mode = { "o", "x" } } },
+		config = function()
+			vim.cmd([[
+        omap     <silent> m :<C-U>lua require('tsht').nodes()<CR>
+        xnoremap <silent> m :lua require('tsht').nodes()<CR>
+      ]])
+		end,
+	},
+
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "BufReadPre",
+    config = true,
+  },
+
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		enabled = false,
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				textobjects = {
+					select = {
+						enable = false,
+						keymaps = {
+							["a,"] = "@parameter.outer",
+							["i,"] = "@parameter.inner",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_end = {
+							["]]"] = "@function.outer",
+							["]["] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[["] = "@function.outer",
+							["[]"] = "@class.outer",
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"p00f/nvim-ts-rainbow",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				rainbow = {
+					enable = true,
+					-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+					extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+					max_file_lines = nil, -- Do not enable for files with more than n lines, int
+					-- colors = {}, -- table of hex strings
+					-- termcolors = {} -- table of colour name strings
+				},
+				require("colorscheme").add_plug_hl({
+					rainbowcol1 = { fg = "#e5c07b", bold = true },
+					rainbowcol2 = { fg = "#8CCBEA", bold = true },
+					rainbowcol3 = { fg = "#A4E57E", bold = true },
+					rainbowcol4 = { fg = "#FF7272", bold = true },
+					rainbowcol5 = { fg = "#FFB3FF", bold = true },
+					rainbowcol6 = { fg = "#9999FF", bold = true },
+				}),
+			})
+		end,
+	},
+
+	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		config = function()
@@ -43,4 +129,5 @@ return {
 				end,
 			})
 		end,
-	}
+	},
+}
