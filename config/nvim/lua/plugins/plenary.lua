1d6caf84d535d327ce0c6362940f5a90c3925032
Shih-Ming Wang <swang150@ucsc.edu>
2023-05-12 21:26:13 -0700

    update

--- config/nvim/lua/plugins/plenary.lua
+++ config/nvim/lua/plugins/plenary.lua
@@ -1,16 +1,16 @@
 return {
-	-- "nvim-lua/plenary.nvim",
-	"ipod825/plenary.nvim",
-	branch = "revert-async-test",
-	config = function()
-		vim.api.nvim_create_autocmd("Filetype", {
-			group = vim.api.nvim_create_augroup("PLENARY", {}),
-			pattern = "lua",
-			callback = function()
-				vim.keymap.set("n", "<F6>", function()
-					require("plenary.test_harness").test_directory(vim.fn.expand("%:p"))
-				end, { desc = "plenary test file" })
-			end,
-		})
-	end,
+    "nvim-lua/plenary.nvim",
+    -- "ipod825/plenary.nvim",
+    -- branch = "revert-async-test",
+    config = function()
+        vim.api.nvim_create_autocmd("Filetype", {
+            group = vim.api.nvim_create_augroup("PLENARY", {}),
+            pattern = "lua",
+            callback = function()
+                vim.keymap.set("n", "<F6>", function()
+                    require("plenary.test_harness").test_directory(vim.fn.expand("%:p"))
+                end, { desc = "plenary test file" })
+            end,
+        })
+    end,
 }
