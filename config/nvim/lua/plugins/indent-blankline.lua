a4b04473e3d45a7aca1e2913dc751e751c475573
Shih-Ming Wang <swang150@ucsc.edu>
2023-11-21 07:38:56 -0800

    update

--- config/nvim/lua/plugins/indent-blankline.lua
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -3,12 +3,12 @@ return{
 		config = function()
 			vim.g.indent_blankline_use_treesitter = true
 			vim.g.indentLine_fileTypeExclude = { "tex", "markdown", "txt", "startify", "packer" }
-			require("colorscheme").add_plug_hl({
-				"IndentBlanklineChar",
-				"IndentBlanklineSpaceChar",
-				"IndentBlanklineSpaceCharBlankline",
-				"IndentBlanklineContextChar",
-				"IndentBlanklineContextStart",
-			})
+			-- require("colorscheme").add_plug_hl({
+			-- 	"IndentBlanklineChar",
+			-- 	"IndentBlanklineSpaceChar",
+			-- 	"IndentBlanklineSpaceCharBlankline",
+			-- 	"IndentBlanklineContextChar",
+			-- 	"IndentBlanklineContextStart",
+			-- })
 		end,
 	} 
